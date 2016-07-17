d235c10c81ab25f458a6dd99a1cb5e187911b8b7
Sahar Hashai <sahar@zeitgold.com>
2016-07-17 18:07:20 +0200

    test fail

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
+import unittest
 
-def main():
-    return True
 
-if __name__ == '__main__':
-    main() 
+class TravisTest(unittest.TestCase):
+    def test_fail(self):
+        self.assertEqual(1, 0)
+
+    def test_success(self):
+        self.assertEqual(1, 1)
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TravisTest)
+unittest.TextTestRunner(verbosity=2).run(suite)
