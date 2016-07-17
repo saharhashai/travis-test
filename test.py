f7a9560d3273cea467202a0b73727fc67639e091
Sahar Hashai <sahar@zeitgold.com>
2016-07-18 00:04:08 +0200

    test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sys
 
 class TravisTest(unittest.TestCase):
     def test_fail(self):
-        self.assertEqual(1, 2)
+        self.assertEqual(1, 0)
 
     def test_success(self):
         self.assertEqual(1, 1)
